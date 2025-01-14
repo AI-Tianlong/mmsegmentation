45b3dfd0be8db4671047a9496e7aecafcdcc40f8
AI-Tianlong <839290771@qq.com>
2025-01-14 11:38:23 +0800

    20250114-浪潮

--- configs_new/ATL-paper-new-hiera/BEiTAdapter-baseline/S2-BEiTAdapter-B-UpetNet-20250114-baseline.py
+++ configs_new/ATL-paper-new-hiera/BEiTAdapter-baseline/S2-BEiTAdapter-B-UpetNet-20250114-baseline.py
@@ -164,7 +164,7 @@ param_scheduler = [
     )
 ]
 
-train_cfg.update(type=IterBasedTrainLoop, max_iters=80000, val_interval=4000)
+train_cfg.update(type=IterBasedTrainLoop, max_iters=80000, val_interval=8000)
 default_hooks.update(
     timer=dict(type=IterTimerHook),
     logger=dict(type=LoggerHook, interval=50, log_metric_by_epoch=False),
